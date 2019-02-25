6a9e4c2b1471d29b18d8ab8586017f3a0c275639
Michael <michael.perezmurillo@gmail.com>
2019-02-24 19:03:03 -0600

    UI's transitions and structure created

--- src/main/resources/static/js/controller.js
+++ src/main/resources/static/js/controller.js
@@ -6,4 +6,28 @@
 
 //init components
 
+function spineRoulete() {
+    var roulette = document.getElementById("roulette");
+    roulette.classList.add("spine-image");
 
+    setTimeout(function () {
+        console.log("temrino de girar");
+
+        var n = document.getElementById("roullete-section");
+        n.classList.add("scale-out");
+
+        setTimeout(function () {
+            n.style.display = "none";
+
+            n = document.getElementById("question-section");
+            n.classList.add("scale-in");
+        }, 500);
+
+    }, 3100);
+
+}
+
+function checkAnswer(){
+    var result = document.getElementById("question-result-container");
+    result.classList.add("scale-in");
+}
